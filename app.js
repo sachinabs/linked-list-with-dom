6da56a81d2c766c79a7a75f865468688dae00a70
sachinabs <sachinabs.js@gmail.com>
2022-03-27 04:20:41 +0530

    new

--- app.js
+++ app.js
@@ -67,6 +67,6 @@ app.post('delete-middle', (req, res) => {
 })
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 })
